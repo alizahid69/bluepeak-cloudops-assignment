41bf1c8406ba4773bb92b80db7551acc59474aff
zahid <alizahid069@gmail.com>
2026-08-02 19:09:12 +0530

    fix: configure service access for KMS keys

--- modules/kms/main.tf
+++ modules/kms/main.tf
@@ -3,58 +3,123 @@ data "aws_partition" "current" {}
 data "aws_region" "current" {}
 
 locals {
-  purposes = toset(["ebs", "rds", "ssm", "secrets", "logs"])
+  purposes = toset([
+    "ebs",
+    "rds",
+    "ssm",
+    "secrets",
+    "logs"
+  ])
 }
 
-dynamic "statement" {
-  for_each = each.key == "ebs" ? [1] : []
+data "aws_iam_policy_document" "this" {
+  for_each = local.purposes
 
-  content {
-    sid    = "AllowAutoScalingServiceLinkedRoleUseOfEbsKey"
+  statement {
+    sid    = "EnableAccountAdministration"
     effect = "Allow"
 
     principals {
       type = "AWS"
 
       identifiers = [
-        "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
+        "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:root"
       ]
     }
 
-    actions = [
-      "kms:Encrypt",
-      "kms:Decrypt",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:DescribeKey"
-    ]
-
+    actions   = ["kms:*"]
     resources = ["*"]
   }
-}
 
-dynamic "statement" {
-  for_each = each.key == "ebs" ? [1] : []
+  # CloudWatch Logs can use only the logs CMK.
+  dynamic "statement" {
+    for_each = each.key == "logs" ? [1] : []
 
-  content {
-    sid    = "AllowAutoScalingServiceLinkedRoleGrantForEbs"
-    effect = "Allow"
+    content {
+      sid    = "AllowCloudWatchLogsEncryption"
+      effect = "Allow"
 
-    principals {
-      type = "AWS"
+      principals {
+        type = "Service"
 
-      identifiers = [
-        "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
+        identifiers = [
+          "logs.${data.aws_region.current.region}.amazonaws.com"
+        ]
+      }
+
+      actions = [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:DescribeKey"
       ]
+
+      resources = ["*"]
+
+      condition {
+        test     = "ArnLike"
+        variable = "kms:EncryptionContext:aws:logs:arn"
+
+        values = [
+          "arn:${data.aws_partition.current.partition}:logs:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/ec2/${var.name_prefix}/*"
+        ]
+      }
     }
+  }
 
-    actions   = ["kms:CreateGrant"]
-    resources = ["*"]
+  # Auto Scaling can use the EBS CMK to launch encrypted instances.
+  dynamic "statement" {
+    for_each = each.key == "ebs" ? [1] : []
 
-    condition {
-      test     = "Bool"
-      variable = "kms:GrantIsForAWSResource"
-      values   = ["true"]
+    content {
+      sid    = "AllowAutoScalingServiceLinkedRoleUseOfEbsKey"
+      effect = "Allow"
+
+      principals {
+        type = "AWS"
+
+        identifiers = [
+          "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
+        ]
+      }
+
+      actions = [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:DescribeKey"
+      ]
+
+      resources = ["*"]
+    }
+  }
+
+  # Auto Scaling requires a KMS grant for AWS-managed EBS resources.
+  dynamic "statement" {
+    for_each = each.key == "ebs" ? [1] : []
+
+    content {
+      sid    = "AllowAutoScalingServiceLinkedRoleGrantForEbs"
+      effect = "Allow"
+
+      principals {
+        type = "AWS"
+
+        identifiers = [
+          "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
+        ]
+      }
+
+      actions   = ["kms:CreateGrant"]
+      resources = ["*"]
+
+      condition {
+        test     = "Bool"
+        variable = "kms:GrantIsForAWSResource"
+        values   = ["true"]
+      }
     }
   }
 }
